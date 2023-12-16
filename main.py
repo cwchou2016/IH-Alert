69021344b9fa25fa033e6dc45c282d83524c7196
cwchou <cwchou@outlook.com>
2023-12-17 05:17:35 +0800

    add WatchFolder implent signal and connect

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
 import sys
 
 from PyQt5 import QtWidgets, uic
+from PyQt5.QtCore import QThread, pyqtSignal
+from time import sleep
+
+
+class WatchFolder(QThread):
+    WATCHING = pyqtSignal(str)
+    FINISHED = pyqtSignal(str)
+
+    def __init__(self):
+        super().__init__()
+        self._running = False
+
+    def run(self):
+        self._running = True
+        while self._running:
+            self.WATCHING.emit("running")
+            sleep(1)
+        self.FINISHED.emit("finished")
+
+    def stop(self):
+        self._running = False
 
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("mainWindow.ui", self)
+
+        self._watch = WatchFolder()
+        self._watch.WATCHING.connect(self.update_plain_text)
+        self._watch.FINISHED.connect(self.update_plain_text)
 
         self.pushButton_start.clicked.connect(self.btn_start_clicked)
         self.pushButton_stop.clicked.connect(self.btn_stop_clicked)
@@ -15,9 +40,11 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def btn_start_clicked(self):
         self.update_status_bar("start clicked")
+        self._watch.start()
         self.update_plain_text("start clicked")
 
     def btn_stop_clicked(self):
+        self._watch.stop()
         self.update_status_bar("stop clicked")
         self.update_plain_text("stop clicked")
 

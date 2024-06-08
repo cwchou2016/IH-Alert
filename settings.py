25215d0507044cbd82bf802e9a909028d6756e37
cwchou <cwchou@outlook.com>
2024-06-08 21:59:45 +0800

    annotate type

--- settings.py
+++ settings.py
@@ -7,10 +7,15 @@ class Settings:
         self._f_name = f_name
         self._config = ConfigParser()
         self._config.add_section("setting")
-        self._options = ["ih_folder", "lis_folder", "complete_sound", "alert_sound", "alert_wait"]
+        self._options = ["ih_folder",
+                         "lis_folder",
+                         "complete_sound",
+                         "alert_sound",
+                         "alert_wait"]
         self.reset()
+        self.load()
 
-    def reset(self):
+    def reset(self) -> None:
         init_values = {
             "ih_folder": "",
             "lis_folder": "",
@@ -21,27 +26,27 @@ class Settings:
 
         self.update(init_values)
 
-    def load(self):
+    def load(self) -> None:
         if not path.isfile(self._f_name):
             return
 
         self._config.read(self._f_name)
 
-    def update(self, value_dict):
+    def update(self, value_dict: dict) -> None:
         for opt in self._options:
             if value_dict.get(opt) is None:
                 continue
 
             self._config.set("setting", opt, value_dict[opt])
 
-    def save(self):
+    def save(self) -> None:
         with open(self._f_name, "w") as f:
             self._config.write(f)
 
-    def get(self, opt):
+    def get(self, opt: str) -> str:
         return self._config.get("setting", opt)
 
-    def get_values(self):
+    def get_values(self) -> dict:
         value_dict = {}
         for opt in self._options:
             value_dict[opt] = self._config.get("setting", opt)
@@ -52,6 +57,5 @@ class Settings:
 if __name__ == "__main__":
     s = Settings("config.ini")
     s.load()
-    # s.update({"lis_folder": "sdf"})
     s.save()
-    print(s.get("sdf"))
+    print(s.get("ih_folder"))

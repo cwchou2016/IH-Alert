7562db1dbdf005f45f3d1b081e831f33920360c8
cwchou <cwchou@outlook.com>
2024-06-08 21:40:35 +0800

    create update()

--- settings.py
+++ settings.py
@@ -6,12 +6,21 @@ class Settings:
     def __init__(self, f_name):
         self._f_name = f_name
         self._config = ConfigParser()
+        self._config.add_section("setting")
+        self._options = ["ih_folder", "lis_folder", "complete_sound", "alert_sound", "alert_wait"]
 
     def load(self):
         if not path.isfile(self._f_name):
             return
 
         self._config.read(self._f_name)
+
+    def update(self, value_dict):
+        for opt in self._options:
+            if value_dict.get(opt) is None:
+                continue
+
+            self._config.set("setting", opt, value_dict[opt])
 
     def save(self):
         with open(self._f_name, "w") as f:
@@ -21,5 +30,6 @@ class Settings:
 if __name__ == "__main__":
     s = Settings("config.ini")
     s.load()
+    s.update({"lis_folder": "sdf"})
     s.save()
     print((s._config))
